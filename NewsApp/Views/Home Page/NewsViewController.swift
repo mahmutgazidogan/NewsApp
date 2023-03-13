86072a5ef1d09d5cf119cc2c930e900e6d147e4b
mahmutgazidogan <m.gazi.dogann>
2023-03-13 22:39:04 +0300

    model and homepage storyboard are ready

---
+++ NewsApp/Views/Home Page/NewsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewsViewController.swift
+//  NewsApp
+//
+//  Created by Mahmut Gazi Doğan on 13.03.2023.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
